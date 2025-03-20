32098cd81a6cf8ff9b1a1d1fb5a6587c8928c7c7
idknowwhy123 <chitipat.put@gmail.com>
2025-03-20 22:36:58 +0700

    edit cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const limiter = rateLimit({
 })
 
 const corsOptions = {
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
 };
